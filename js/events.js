3d32ed05c09e0d7c7a7c02bf9e0b6d896c444d67
Aswin <jayaprakash.jp713@gmail.com>
2023-11-23 17:08:16 +0530

    About Update

--- js/events.js
+++ js/events.js
@@ -1,1 +1,17 @@
-//javascript
+//obsrever code
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => { //looping over the entires
+        console.log(entry)
+        if(entry.isIntersecting){ //checking if entry intersects the viewport
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show') //for multiple times
+        }
+    })
+})  
+//IntersectionObserver can observe multiple elements/entries at the same time, Here Scroll Aimationz
+
+const hidden = document.querySelectorAll('.hidden') 
+hidden.forEach((el) => observer.observe(el)) //what to observe 
+ 
